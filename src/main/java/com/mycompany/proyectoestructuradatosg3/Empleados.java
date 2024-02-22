527c33485d6e29d4b2514d1de406c13944ed6977
arleyyz <134114607+arleyyz@users.noreply.github.com>
2024-02-21 20:33:49 -0600

    Update Empleados.java
    Adding comments of Empleados class. AAA

--- src/main/java/com/mycompany/proyectoestructuradatosg3/Empleados.java
+++ src/main/java/com/mycompany/proyectoestructuradatosg3/Empleados.java
@@ -9,22 +9,23 @@ package com.mycompany.proyectoestructuradatosg3;
  * @author anchi
  */
 public class Empleados {
-    private NodoP cima;
-
+    private NodoP cima; // referencia al nodo superior de la pila
+    
+// constructor inicializa la cima de la pila a null
     public Empleados() {
         this.cima = null;
     }
-
+// ggetter para obtener la cima de la pila
     public NodoP getCima() {
         return cima;
     }
-
+// stter para establecer la cima de la pila
     public void setCima(NodoP cima) {
         this.cima = cima;
     }
 
     
-    
+     // verifica si la pila esta vacia
     public boolean esVacia()
     {
         if(cima == null)
@@ -36,43 +37,43 @@ public class Empleados {
             return false;
         }
     }
-    
+    // almacena un nuevo empleado en la pila
     public void almacenarEmpleado(int id, String nombre, String puesto)
     {
-        NodoP nuevoEmpleado = new NodoP(id, nombre, puesto);
-        if(esVacia())
+        NodoP nuevoEmpleado = new NodoP(id, nombre, puesto); // crea un nuevo nodo empleado
+        if(esVacia()) // si la pila esta vacia, el nuevo nodo se convierte en la cima
         {
             cima = nuevoEmpleado;
-        }
-        else
-        {
+        
+        } else { // si no, el nuevo nodo se pone encima de la pila actual
+        
             nuevoEmpleado.setSiguienteP(cima); 
             cima = nuevoEmpleado;
         }
     }
-    
+    // imprime todos los empleados usanmdo la recursividad
     public String imprimirEmpleados(NodoP nodo, String result) {
-        result = nodo.toString() + "\n";
-        if (nodo.getSiguienteP() != null) {
+        result = nodo.toString() + "\n"; // agrega la informacion del nodo actual al resultado
+        if (nodo.getSiguienteP() != null) { //si hay mas nodos en la pila
             
-            result = nodo.toString() + "\n";
+            result = nodo.toString() + "\n"; // llama al metodo recursivamente con el siguiente nodo
             result += imprimirEmpleados(nodo.getSiguienteP(), result);
         }
-         return result;
+         return result; //return para devolver el resultado
     }
     
     
     
-    
+    //convierte la lista de empleados a una cadena de texto
     @Override
     public String toString(){
         String s = "";
-        NodoP aux = cima;
-        while (aux != null) {
-            s += aux + "\n";
-            aux = aux.getSiguienteP();
+        NodoP aux = cima; // empieza por la cima de la pila
+        while (aux != null) { // recorre todos los nodos de la pila
+            s += aux + "\n"; // agrega la informacion de cada nodo a la cadena
+            aux = aux.getSiguienteP(); // pasa al siguiente nodo
         }
-        return s;
+        return s; //devuelve s
     
     }
     
